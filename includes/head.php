880d5ed4898f131f7248af24a85b244af8811327
Avik Nigam <business.aviknigam@gmail.com>
2018-01-27 23:27:22 +1100

    Added head tag includes

---
+++ includes/head.php
@@ -0,0 +1,44 @@
+<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <title><?= 'Uni Study Mate | ' .$title ?></title>
+        <meta name="description" content="<?= $description ?>">
+        <meta name="author" content="Uni Study Mate">
+
+        <!-- Favicon -->
+        <link rel="apple-touch-icon" sizes="180x180" href="/dist/img/favicon/apple-touch-icon.png?v=kPg5dmjPmB">
+        <link rel="icon" type="image/png" sizes="32x32" href="/dist/img/favicon/favicon-32x32.png?v=kPg5dmjPmB">
+        <link rel="icon" type="image/png" sizes="16x16" href="/dist/img/favicon/favicon-16x16.png?v=kPg5dmjPmB">
+        <link rel="manifest" href="/dist/img/favicon/manifest.json?v=kPg5dmjPmB">
+        <link rel="mask-icon" href="/dist/img/favicon/safari-pinned-tab.svg?v=kPg5dmjPmB" color="#3a004d">
+        <link rel="shortcut icon" href="/dist/img/favicon/favicon.ico?v=kPg5dmjPmB">
+        <meta name="theme-color" content="#ffffff">
+
+        <!-- Google Analytics -->
+        <?php
+            if ($googleAnalytics == 1) {
+                include 'head.analytics.php';
+            };
+        ?>
+
+        <!-- Facebook -->
+        <meta property="og:title" content="<?= 'Uni Study Mate | ' .$title ?>">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="<?= $configurl ?>">
+        <meta property="og:description" content="<?= $description ?>">
+        <meta property="og:image" content="<?= $configurl ?>/dist/img/og.jpg">
+
+        <!-- Twitter -->
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="<?= 'Uni Study Mate | ' .$title ?>" />
+        <meta name="twitter:description" content="<?= $description ?>" />
+        <meta name="twitter:image:src" content="<?= $configurl ?>/dist/img/favicon/android-chrome-256x256.jpg" />
+
+        <!-- Normalize CSS -->
+        <link rel="stylesheet" href="https://necolas.github.io/normalize.css/7.0.0/normalize.css">
+        <!-- Font Awesome -->
+        <link href="https://use.fontawesome.com/releases/v5.0.1/css/all.css" rel="stylesheet">
+        <!-- Custom CSS -->
+        <link rel="stylesheet" type="text/css" href="/assets/css/app.css?<?php echo time(); ?>">
+        <link rel="stylesheet" type="text/css" href="/assets/css/responsive.css?<?php echo time(); ?>">
