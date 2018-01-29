6256fa9590762f9390bf48d47afeb6a79154db73
Avik Nigam <business.aviknigam@gmail.com>
2018-01-29 17:46:52 +1100

    Added scroll sript

---
+++ textbooks/index.php
@@ -0,0 +1,36 @@
+<?php
+require __DIR__ . '/../core/init.php';
+$title = 'Buy and Sell Textbooks';
+$description = 'Buy and sell university textbooks for affordable prices.';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include '../includes/head.php'; ?>
+    </head>
+
+    <body>
+        <!-- Navbar -->
+            <?php include '../includes/navbar.php'; ?>
+
+        <!-- Landing -->
+            <div class="container landing flex">
+                <div class="left flex">
+                    <h1 class="landing-heading">Textbook Marketplace</h1>
+                    <p>Australia's fastest growing online tertiary student community. Buy and sell textbooks at NO COMMISSION!</p>
+                </div>
+
+                <div class="right flex">
+                    <h1 class="landing-heading">Search</h1>
+                </div>
+            </div>
+        
+        
+        <!-- Scripts -->
+            <?php include '../includes/scripts.php'; ?>
+    </body>
+</html>
+
+<!--                     
+ -->
