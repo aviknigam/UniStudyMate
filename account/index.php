5941ab6bc511f5a000065a2b76027b309229d14f
Avik Nigam <business.aviknigam@gmail.com>
2018-02-02 23:50:33 +1100

    Added support for individual user accounts and option to sell textbooks

--- account/index.php
+++ account/index.php
@@ -1,20 +1,30 @@
 <?php
-    require __DIR__ . '/../core/init.php';
-    $title = 'Buy and Sell Textbooks';
-    $description = 'Buy and sell university textbooks for affordable prices.';
+	require __DIR__ . '/../core/init.php';
+	$title = 'Account';
+	$description = 'Account';
+	$navbar = 'account';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <?php include '../includes/head.php'; ?>
-        <link rel="stylesheet" type="text/css" href="/dist/css/textbooks.css?<?php echo time(); ?>">
-    </head>
+	<head>
+		<?php include '../includes/head.php'; ?>
+		<link rel="stylesheet" type="text/css" href="/dist/css/account.css?<?php echo time(); ?>">
+	</head>
 
-    <body>
-        <!-- Navbar -->
-            <?php include '../includes/navbar.php'; ?>
+	<body>
+		<!-- Navbar -->
+			<?php include '../includes/navbar.php'; ?>
 
-        <!-- Landing -->
-            <div class="page-section bg-blue">
-                <div class="container">
+		<!-- Landing -->
+			<div class="page-section bg-blue">
+				<div class="container">
+				
+				</div>
+			</div>
+
+		<!-- Scripts -->
+			<?php include 'includes/scripts.php'; ?>
+			
+	</body>
+</html>
