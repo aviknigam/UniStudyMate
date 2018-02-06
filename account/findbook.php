30a9fb46e7f34b66385b900f0a94646be1c44a41
Avik Nigam <business.aviknigam@gmail.com>
2018-02-06 22:39:16 +1100

    Can now search a book using external sites by file_get_contents

--- account/findbook.php
+++ account/findbook.php
@@ -16,25 +16,40 @@ $sql->execute();
 // Need get_result() to give the result to
 $result = $sql->get_result();
 
-// Exit if we cannot find the post in SQL
-if(!$result->num_rows > 0) {
-	header("Location: /404");
-    exit;
+// If we cannot find the post in the database
+if($result->num_rows > 0) {
+    $html1 = file_get_contents('https://isbndb.com/book/' . $_GET['url']);
+    $html2 = file_get_contents('https://studentvip.com.au/textbooks/' . $_GET['url']);
+	
+    // Url doesn't allow file_get_contents
+    // $html3 = file_get_contents('http://isbnsearch.org/isbn/' . $_GET['url']);
+    
+    preg_match("'<h1 class=\"textbook-title h3\"><strong>(.*?)</strong>'si", $html2, $textbookTitle);
+
+    if (!preg_match("'<th>Publish Date</th> <td>(.*?)-'si", $html1, $textbookYear)) {
+        $textbookYear = '';
+    }
+
+    if (!preg_match("'<h5 class=\"textbook-author\">(.*?)</h5>'si", $html2, $textbookAuthor)) {
+        // $textbookAuthor = '-';
+        echo "no author";
+    } else {
+        $textbookAuthor = $textbookAuthor[1];
+    }
+    
+    if(!preg_match("'<th>Edition</th> <td>(.*?)</td>'si", $html1, $textbookEdition)) {
+        $textbookEdition = '-';
+    }
+
+    preg_match("'<object height=\"250px\" width=\"190px\" data=\"(.*?)\" type='si", $html1, $textbookURL);
+    
+    echo "$textbookTitle[1] <br> $textbookAuthor <br> $textbookYear[1] <br> $textbookEdition[1] <br> <img src=\"$textbookURL[1]\"> <br>";
 }
 
-// $html = file_get_contents('https://isbndb.com/book/' . $_GET['url']);
 
-// Url doesn't allow file_get_contents
-// $html = file_get_contents('http://isbnsearch.org/isbn/' . $_GET['url']);
-
-preg_match("'<h1>(.*?)</h1>'si", $html, $textbookTitle);
-preg_match("'<p><strong>Published:</strong>^(\s*.*?\s).*'si", $html, $textbookYear);
-preg_match("'<p><strong>Authors:</strong> (.*?)</p>'si", $html, $textbookAuthor);
-preg_match("'<p><strong>Edition:</strong> (.*?)</p>'si", $html, $textbookEdition);
-preg_match("'<img src=\"(.*?)\" alt'si", $html, $textbookURL);
-
-echo "$textbookTitle[1] <br> $textbookAuthor[1] <br> $textbookYear[1] <br> $textbookEdition[1] <br> $textbookURL[1] <br>";
 
 
 
 ?>
+
+<!-- (.*?)^(\s*.*?\s).*'si" -->
