1b0bf0abddcd6b03ef16da2c49e1d86087e919e1
Avik Nigam <business.aviknigam@gmail.com>
2018-02-06 22:40:01 +1100

    major changes to the AJAX loader and displaying search results

--- account/sell.php
+++ account/sell.php
@@ -9,7 +9,7 @@
 <html lang="en">
 	<head>
 		<?php include '../includes/head.php'; ?>
-		<!-- <link rel="stylesheet" type="text/css" href="/dist/css/account.css?<?php echo time(); ?>"> -->
+		<link rel="stylesheet" type="text/css" href="/dist/css/account.css?<?php echo time(); ?>">
 	</head>
 
 	<body>
@@ -17,21 +17,32 @@
 			<?php include '../includes/navbar.php'; ?>
 
 		<!-- Landing -->
+			<div class="page-section bg-blue">
+				<div class="container landing">
+					<h1 class="h-white landing-heading">Sell a Textbook</h1>
+				</div>
+			</div>			
+
+		<!-- Sell Textbook -->
 			<div class="page-section">
-				<div class="container">
+				<div class="container flex ffcolwrap align-items-center">
                     <form id="textbookSearch">
-                        <label for="textbookISBN">Textbook ISBN (no dashes): </label>
+                        <label for="textbookISBN">Enter 13 digit textbook ISBN (no dashes): </label>
                         <input type="text" name="textbookISBN" id="textbookISBN">
-                        <button id="search">Search</button>
-                    </form>
+                        <button class="btn btn-dark">Search</button>
+					</form>
+					<div id="loader" class="hide">
+						<img src="/dist/img/loader.gif" alt="Loading results...">
+					</div>
 				</div>
 			</div>
         
         <!-- Search Results -->
             <div class="page-section">
                 <div class="container">
-                    <div id="searchResults">
-
+                    <div id="searchResults" class="flex ffcolwrap align-items-center">
+						<img src="https://isbnsearch.org/images/isbn-location.png" alt="The ISBN is typically found above the barcode">
+						<p>Typical location of an ISBN is on the back of the book.</p>
                     </div>
                 </div>
             </div>
