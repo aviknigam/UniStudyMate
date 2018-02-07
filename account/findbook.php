71135661b7ad9d7d92606c4d9b561c79555bc4c0
Avik Nigam <business.aviknigam@gmail.com>
2018-02-07 23:18:49 +1100

    Can now insert into MySQL database if the book is not already there

--- account/findbook.php
+++ account/findbook.php
@@ -17,39 +17,64 @@ $sql->execute();
 $result = $sql->get_result();
 
 // If we cannot find the post in the database
-if($result->num_rows > 0) {
-    $html1 = file_get_contents('https://isbndb.com/book/' . $_GET['url']);
-    $html2 = file_get_contents('https://studentvip.com.au/textbooks/' . $_GET['url']);
+if(!$result->num_rows > 0) {
+	$html1 = file_get_contents('https://isbndb.com/book/' . $_GET['url']);
 	
-    // Url doesn't allow file_get_contents
-    // $html3 = file_get_contents('http://isbnsearch.org/isbn/' . $_GET['url']);
-    
-    preg_match("'<h1 class=\"textbook-title h3\"><strong>(.*?)</strong>'si", $html2, $textbookTitle);
+	// URL is for backup
+	// $html2 = file_get_contents('https://studentvip.com.au/textbooks/' . $_GET['url']);
 
-    if (!preg_match("'<th>Publish Date</th> <td>(.*?)-'si", $html1, $textbookYear)) {
-        $textbookYear = '';
-    }
+	// URL doesn't allow file_get_contents
+	// $html3 = file_get_contents('http://isbnsearch.org/isbn/' . $_GET['url']);
+	
+	if (!preg_match("'<th>Full Title</th> <td>(.*?)</td>'si", $html1, $textbookTitle)) {
+		$textbookTitle = '-';
+	} else {
+		$textbookTitle = $textbookTitle[1];
+	}
 
-    if (!preg_match("'<h5 class=\"textbook-author\">(.*?)</h5>'si", $html2, $textbookAuthor)) {
-        // $textbookAuthor = '-';
-        echo "no author";
-    } else {
-        $textbookAuthor = $textbookAuthor[1];
-    }
-    
-    if(!preg_match("'<th>Edition</th> <td>(.*?)</td>'si", $html1, $textbookEdition)) {
-        $textbookEdition = '-';
-    }
+	if (!preg_match("'<th>Publish Date</th> <td>(.*?)-'si", $html1, $textbookYear)) {
+		$textbookYear = '-';
+	} else {
+		$textbookYear = $textbookYear[1];
+	}
 
-    preg_match("'<object height=\"250px\" width=\"190px\" data=\"(.*?)\" type='si", $html1, $textbookURL);
-    
-    echo "$textbookTitle[1] <br> $textbookAuthor <br> $textbookYear[1] <br> $textbookEdition[1] <br> <img src=\"$textbookURL[1]\"> <br>";
+	if (!preg_match_all("'<a href=\"/author/(.*?)\">'si", $html1, $textbookAuthor)) {
+		$textbookAuthor = '-';
+	} else {
+		$textbookAuthor = join("; ", $textbookAuthor[1]);
+	}
+
+	if (!preg_match("'<th>Edition</th> <td>(.*?)</td>'si", $html1, $textbookEdition)) {
+		$textbookEdition = '';
+	} else {
+		$textbookEdition = $textbookEdition[1];
+	}
+
+	if (!preg_match("'<object height=\"250px\" width=\"190px\" data=\"(.*?)\"'si", $html1, $textbookURL)) {
+		$textbookURL = '';
+	} else {
+		$textbookURL = $textbookURL[1];
+	}
+	
+	// Insert into database
+	$sql =	$conn->prepare("INSERT INTO textbooks (textbookISBN, textbookTitle, textbookYear, textbookAuthor, textbookEdition, textbookURL) VALUES (?, ?, ?, ?, ?, ?)");
+	$sql->bind_param("ssssss", $textbookISBN, $textbookTitle, $textbookYear, $textbookAuthor, $textbookEdition, $textbookURL);
+	$sql->execute();
+
+	echo "
+		<div class='flex justify-content-center'>
+			<img src='$textbookURL' class='search-img' alt='A picture for $textbookTitle is not available at the moment'>
+			<table class='table'>
+				<tbody>
+					<tr><td><strong>Title</strong></td> <td>$textbookTitle</td>
+					<tr><td><strong>Year</strong></td> <td>$textbookYear</td>
+					<tr><td><strong>Authors</td></strong> <td>$textbookAuthor</td>
+					<tr><td><strong>Edition</strong></td> <td>" .ordinal($textbookEdition). "</td>
+				</tbody>
+			</table>
+		</div>";
+
 }
 
 
-
-
-
 ?>
-
-<!-- (.*?)^(\s*.*?\s).*'si" -->
