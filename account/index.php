fc5b3c60a6f4d6db9854e35d08f778be772c47d4
Avik Nigam <business.aviknigam@gmail.com>
2018-02-09 02:35:58 +1100

    Log in support is now here, users can log in and log out of their account

--- account/index.php
+++ account/index.php
@@ -1,8 +1,39 @@
 <?php
 	require __DIR__ . '/../core/init.php';
-	$title = 'Account';
-	$description = 'Account';
+	$title = 'View Account';
+	$description = 'List your textbooks for sale, add subject reviews and advertise yourself as a University tutor!';
 	$navbar = 'account';
+
+	if(isset($_POST['submit'])) {
+		$userEmail = sanitize($_POST['email']);
+		$userPassword = sanitize($_POST['password']);
+		
+		$sql = $conn->prepare("SELECT * FROM users WHERE userEmail = ?");
+		$sql->bind_param("s", $userEmail);
+		$sql->execute();
+
+		$row_find_hash = ($sql->get_result())->fetch_assoc();
+		$hash_password = $row_find_hash['userPassword'];
+		$hash = password_verify($userPassword, $hash_password);
+
+		// Check row association 
+		$stmt_sql_login_user = $conn->prepare("SELECT * FROM users WHERE userEmail = ? AND userPassword = ?");
+		$stmt_sql_login_user->bind_param("ss", $userEmail, $hash_password);
+		$stmt_sql_login_user->execute();
+
+
+		// Run Check IF statement
+		if ($hash == 0) {
+			echo 'Incorrect email or password, please <a href="/account/" style="color: blue;">try logging in again.</a> If you are not a member yet then you can <a href="/account/register" style="color: blue;">Sign up for free!</a>';
+
+		} elseif (!$row_login_user = ($stmt_sql_login_user->get_result())->fetch_assoc()) {
+			echo 'Incorrect email or password, please <a href="/account/" style="color: blue;">try logging in again.</a> If you are not a member yet then you can <a href="/account/register" style="color: blue;">Sign up for free!</a>';
+
+		} else { 
+			$_SESSION['userID'] = $row_login_user['userID'];
+			header("Location: ./");
+		}
+	}
 ?>
 
 <!DOCTYPE html>
@@ -19,7 +50,19 @@
 		<!-- Landing -->
 			<div class="page-section">
 				<div class="container">
-				
+					<!-- If not logged in, display login form -->
+						<?php
+							if(!isset($_SESSION['userID'])) {
+								echo '
+									<h1 class="h-grey landing-heading">Log in to <span class="brand">UniStudyMate:</span></h1>
+									<form action="" method="POST" class="login-form">
+										<input type="email" name="email" placeholder="Email address" required autofocus>
+										<input type="password" name="password" placeholder="Password" required>
+										<button class="btn btn-dark btn-block" name="submit">Login</button>
+									</form>
+								';
+							}
+						?>
 				</div>
 			</div>
 
