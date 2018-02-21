6e41e743171d40eeb14f2a32fe911b7a63f65146
Avik Nigam <business.aviknigam@gmail.com>
2018-02-21 23:55:41 +1100

    Can now delete textbooks from the account

---
+++ account/delete.php
@@ -0,0 +1,19 @@
+<?php
+require __DIR__ . '/../core/init.php';
+
+if (!isset($_SESSION['userID'])) {
+    header('Location: ./login');
+    die();
+} else {
+    $userID = $_SESSION['userID'];
+}
+
+//Gather listingID
+$listingID = sanitize($_GET['listingID']);
+
+//Check is listing exists
+$sql_listings = $conn->prepare("DELETE FROM listings WHERE listingID = ? AND user_id = ?");
+$sql_listings->bind_param("ss", $listingID, $userID);
+$sql_listings->execute();
+
+header('Location: ./');
