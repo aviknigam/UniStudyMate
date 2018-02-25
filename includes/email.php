2d9b19319715cfa629598f21df4042970b85d53b
Avik Nigam <business.aviknigam@gmail.com>
2018-02-26 00:05:54 +1100

    Added a generic email mailer

---
+++ includes/email.php
@@ -0,0 +1,13 @@
+<?php
+
+// ----- COMPOSE EMAIL ----- 
+$headers = "From: $configTitle <contact@unistudymate.com>\r\n";
+$headers .= "Reply-To: $to\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+$message = wordwrap($message, 70, "\r\n");
+
+mail($to, $subject, $message, $headers);
+
+header ('Location: /includes/email.sent');		
