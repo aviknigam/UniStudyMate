8771aa652312e2b1de7e87987a96463e13726079
Avik Nigam <business.aviknigam@gmail.com>
2018-02-26 23:26:50 +1100

    Added ad support

---
+++ includes/ads-responsive.php
@@ -0,0 +1,16 @@
+<?php
+    if ($ads == 1) {
+        echo '
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <!-- Responsive - Colourful -->
+            <ins class="adsbygoogle"
+                style="display:block"
+                data-ad-client="ca-pub-5552998534475576"
+                data-ad-slot="5509369579"
+                data-ad-format="auto"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        ';
+    }
+?>
