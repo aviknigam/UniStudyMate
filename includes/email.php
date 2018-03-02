5b681048d30a1b897db80117a9440ecc921062b7
Avik Nigam <business.aviknigam@gmail.com>
2018-03-02 23:23:16 +1100

    Changed email functionality, added a from field which was missing

--- includes/email.php
+++ includes/email.php
@@ -2,7 +2,7 @@
 
 // ----- COMPOSE EMAIL ----- 
 $headers = "From: $configTitle <contact@unistudymate.com>\r\n";
-$headers .= "Reply-To: $to\r\n";
+$headers .= "Reply-To: $from\r\n";
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
