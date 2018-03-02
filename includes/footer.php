2c8dfb73cb6a4e458b0d8e52e7ae716f5eef520e
Avik Nigam <business.aviknigam@gmail.com>
2018-03-02 23:16:30 +1100

    Changed the brand size and linked to Facebook page

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,8 @@
 <div class="page-section bg-blue">
-    <div class="container flex footer text-white">
-        <a href="/" class="brand flex align-items-center">UniStudyMate</a>
-        <div class="social">
-            <h2>Facebook</h2>
-        </div>
+    <div class="container flex footer align-items-center text-white">
+        <a href="/"><h2 class="brand">UniStudyMate</h2></a>
+        <a href="https://www.vultr.com/?ref=7307215"><img src="https://www.vultr.com/media/badge_onblue.png" alt="For your university website hosting options, look no further!" class="footer-vultr"></a>
+        <a href="https://www.facebook.com/UniStudyMate-1996676410349463/"><img src="https://96r0bhh5pc-flywheel.netdna-ssl.com/wp-content/uploads/2017/04/facebook-2.png" alt="Like us on Facebook!" class="footer-social"></a>
     </div>
 </div>
 
